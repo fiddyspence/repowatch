a9a89e3d7d6c8da5b7421f988594812f3cea58b0
Chris Spence <chris@spence.org.uk>
2017-03-24 15:58:21 +0000

    initial commit

---
+++ repowatch.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "log"
+  "github.com/fsnotify/fsnotify"
+)
+
+func main() {
+  watcher, err := fsnotify.NewWatcher()
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer watcher.Close()
+
+  done := make(chan bool)
+  go func() {
+    for {
+      select {
+      case event := <-watcher.Events:
+        log.Println("event:", event)
+        if event.Op&fsnotify.Write == fsnotify.Write {
+          log.Println("modified file:", event.Name)
+          log.Println("modified file:", event.Create)
+        }
+        if event.Op&fsnotify.Remove == fsnotify.Remove {
+          log.Println("closed file:", event.Name)
+        }
+      case err := <-watcher.Errors:
+        log.Println("error:", err)
+      }
+    }
+  }()
+
+  err = watcher.Add("/tmp")
+  if err != nil {
+    log.Fatal(err)
+  }
+  <-done
+}
+
+
